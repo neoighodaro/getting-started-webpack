39938ad1aabd5c57b24096cfb7df909b7f4a03da
Neo Ighodaro <neo@creativitykills.co>
2019-01-08 08:26:54 +0100

    Initial part 7

---
+++ Part-7/src/index.js
@@ -0,0 +1,23 @@
+import RandomStringGenerator from './utilities/random';
+import copyToClipboard from './utilities/copyToClipboard';
+import './style.scss';
+import passwordIcon from './images/lock-solid.svg';
+import copyIcon from './images/copy-solid.svg';
+import securityIllustration from './images/security.svg';
+
+document.getElementById('header_image').src = securityIllustration;
+
+const copyIconElem = document.getElementById('copy_icon');
+copyIconElem.src = copyIcon;
+copyIconElem.onclick = () => {
+  copyToClipboard(document.getElementById('actual_password').innerText);
+  alert('Copied to clipboard');
+};
+
+document.getElementById('password_icon').src = passwordIcon;
+
+document.addEventListener('DOMContentLoaded', () => {
+  const randomStringGenerator = new RandomStringGenerator();
+  const randomString = `Random String: <span id="actual_password">${randomStringGenerator.generate()}</span>`;
+  document.getElementById('random_password').innerHTML = randomString;
+});
---
+++ Part-7/webpack.config.js
@@ -0,0 +1,56 @@
+const webpack = require('webpack');
+const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+const env = process.env.NODE_ENV;
+
+module.exports = {
+  mode: env == 'production' || env == 'none' ? env : 'development',
+  entry: path.resolve(__dirname + '/src/index.js'),
+  output: {
+    path: path.resolve(__dirname + '/dist/assets'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.(png|jp(e*)g|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8000,
+              publicPath: 'assets',
+              name: 'images/[hash]-[name].[ext]'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css'
+    }),
+    new OptimizeCssAssetsPlugin({
+      cssProcessorPluginOptions: {
+        preset: ['default', { discardComments: { removeAll: true } }]
+      }
+    })
+  ],
+  optimization: {
+    minimizer: [new UglifyJsPlugin()]
+  }
+};
