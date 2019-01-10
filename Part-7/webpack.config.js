8cad55a3a18f774d6d728d0796cbd255ef652c13
Neo Ighodaro <neo@creativitykills.co>
2019-01-10 16:09:30 +0100

    Updated purgecss

--- Part-7/webpack.config.js
+++ Part-7/webpack.config.js
@@ -1,8 +1,10 @@
 const webpack = require('webpack');
 const path = require('path');
+const glob = require('glob');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const PurgeCssPlugin = require('purgecss-webpack-plugin');
 
 const env = process.env.NODE_ENV;
 
@@ -19,7 +21,19 @@ module.exports = {
     rules: [
       {
         test: /\.scss$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: function() {
+                return [require('precss'), require('autoprefixer')];
+              }
+            }
+          },
+          'sass-loader'
+        ]
       },
       {
         test: /\.js$/,
@@ -53,11 +67,18 @@ module.exports = {
 
 if (env === 'production') {
   module.exports.optimization.minimizer.push(new UglifyJsPlugin());
+
   module.exports.plugins.push(
     new OptimizeCssAssetsPlugin({
       cssProcessorPluginOptions: {
         preset: ['default', { discardComments: { removeAll: true } }]
       }
+    })
+  );
+
+  module.exports.plugins.push(
+    new PurgeCssPlugin({
+      paths: glob.sync(path.join(__dirname, 'src') + '/**/*', { nodir: true })
     })
   );
 }
