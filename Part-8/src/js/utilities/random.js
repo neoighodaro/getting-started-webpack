3fc9b7080f3b3330b54235aabd21fef6a1ec2512
Neo Ighodaro <neo@creativitykills.co>
2019-01-19 15:36:50 +0100

    Updated code

--- Part-8/src/js/utilities/random.js
+++ Part-8/src/js/utilities/random.js
@@ -1,21 +1,13 @@
-const RandomStringGenerator = () => {
-  // generate() {
-  //   return this._rand() + this._rand();
-  // }
+class RandomStringGenerator {
+  generate() {
+    return this._rand() + this._rand();
+  }
 
-  // _rand() {
-  //   return Math.random()
-  //     .toString(36)
-  //     .substring(2, 15);
-  // }
-
-  const _rand = () => {
+  _rand() {
     return Math.random()
       .toString(36)
       .substring(2, 15);
-  };
-
-  return _rand() + _rand();
-};
+  }
+}
 
 export default RandomStringGenerator;
